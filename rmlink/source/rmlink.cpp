29a42f14cddf37e989e61413e912f359b4d8f576
Jean-Philippe Steinmetz <caskater47@gmail.com>
2014-09-19 23:07:32 -0700

    Updating end user license agreement

--- rmlink/source/rmlink.cpp
+++ rmlink/source/rmlink.cpp
@@ -15,10 +15,6 @@
 //   this list of conditions and the following disclaimer in the documentation
 //   and/or other materials provided with the distribution.
 // 
-// * Neither the name of the {organization} nor the names of its
-//   contributors may be used to endorse or promote products derived from
-//   this software without specific prior written permission.
-// 
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 // AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 // IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -203,10 +199,6 @@ void PrintVersion()
 	_tprintf(TEXT("* Redistributions in binary form must reproduce the above copyright notice,\n"));
 	_tprintf(TEXT("  this list of conditions and the following disclaimer in the documentation\n"));
 	_tprintf(TEXT("  and/or other materials provided with the distribution.\n"));
-	_tprintf(TEXT("\n"));
-	_tprintf(TEXT("* Neither the name of the {organization} nor the names of its\n"));
-	_tprintf(TEXT("  contributors may be used to endorse or promote products derived from\n"));
-	_tprintf(TEXT("  this software without specific prior written permission.\n"));
 	_tprintf(TEXT("\n"));
 	_tprintf(TEXT("THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\"\n"));
 	_tprintf(TEXT("AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE\n"));
